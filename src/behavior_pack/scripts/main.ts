b977b47a87c3b9313c3ca4c62bd50036658407b2
Lete114 <me@imlete.cn>
2023-10-13 20:26:16 +0800

    feat: remove subdir

--- src/behavior_pack/scripts/main.ts
+++ src/behavior_pack/scripts/main.ts
@@ -2,5 +2,6 @@ import { world } from '@minecraft/server'
 import { color } from '@mcbe-mods/utils'
 import { module } from './module'
 world.afterEvents.itemUse.subscribe(() => {
-  color.red.italic('5555555', module)
+  // eslint-disable-next-line no-console
+  console.warn(color.red.italic('5555555', module))
 })
